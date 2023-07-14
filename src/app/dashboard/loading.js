f173a685d8d143a199637c979d90ea39a6ff4ac9
Bug Coding <61137331+fawait123@users.noreply.github.com>
2023-07-14 20:58:33 +0700

    update

---
+++ src/app/dashboard/loading.js
@@ -0,0 +1,17 @@
+"use client";
+import { Spin } from "antd";
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Spin size="large" />
+    </div>
+  );
+}
