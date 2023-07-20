709f07082ba9c004e8c9cae3af5f5b07bc1190da
Bug Coding <61137331+fawait123@users.noreply.github.com>
2023-07-20 20:14:54 +0700

    update terbaru

--- src/API/http.js
+++ src/API/http.js
@@ -42,3 +42,49 @@ export const getUser = async (next) => {
     },
   }).then((response) => next(response.data.results.data));
 };
+
+//========================================== API GET SPECIALIST ==============================
+
+export const getSpecialist = async (next) => {
+  const res = await API.get("/admin/specialist", {
+    params: {
+      page: 0,
+      limit: 10,
+    },
+  }).then((response) => next(response.data.results.data));
+};
+
+export const storeSpecialist = async (data, next) => {
+  const res = await API({
+    url: "/admin/specialist",
+    method: "post",
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+    data: data,
+  }).then((response) => next(response.data));
+};
+
+export const updateSpecialist = async ({ formData, id }, next) => {
+  const res = await API({
+    url: "/admin/specialist",
+    method: "put",
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+    data: formData,
+    params: {
+      id: id,
+    },
+  }).then((response) => next(response.data));
+};
+
+export const destroySpecialist = async ({ id }, next) => {
+  const res = await API({
+    url: "/admin/specialist",
+    method: "delete",
+    params: {
+      id: id,
+    },
+  }).then((response) => next(response.data));
+};
