65587fa9018c89c800b0dc481ea1789b89a391a8
gigihprasetia <82494987+gigihprasetia@users.noreply.github.com>
2023-09-13 21:35:56 +0700

    reschedule

--- src/utils/base_url.js
+++ src/utils/base_url.js
@@ -1,6 +1,6 @@
-// export const BASE_URL = "https://api.afiadoc.com";
+export const BASE_URL = "https://api.afiadoc.com";
 // export const BASE_URL = "http://192.168.148.190:8000";
 // export const BASE_URL = "https://5e43-114-10-11-0.ngrok-free.app";
-export const BASE_URL = "http://localhost:8000";
+// export const BASE_URL = "http://localhost:8000";
 // export const BASE_URL = "http://10.10.0.202:8000";
 // export const PATH_IMAGE = `${BASE_URL}/public/uploads`;
